ba5d5acdd1835138b08b3798df2ba9c77f034042
abrar aboobacker <abraraboobackerkk@gmail.com>
2023-04-04 10:43:23 +0530

    get all doctor on beckend

--- src/Components/Admin/Doctors.jsx
+++ src/Components/Admin/Doctors.jsx
@@ -1,6 +1,7 @@
-import { Box, styled} from "@mui/material";
-import React from "react";
+import { Box, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, styled} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../AdminSidebar/SideBar";
+import axios from "../../axios/axios";
 const drawerWidth = 240;
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -11,6 +12,25 @@ const DrawerHeader = styled('div')(({ theme }) => ({
     ...theme.mixins.toolbar,
   }));
 const Doctors = () => {
+  const [doctor,setDoctor]=useState([])
+  const getDoctor =async () =>{
+    try {
+      const response = await axios.get("admin/getAllDoctors",{
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("admintoken")}`,
+        },
+      })
+      if(response.data.success){
+        setDoctor(response.data.data)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getDoctor();
+  }, []);
+  const blockHandler =  ()=>{}
   return (
     <>
      <Box sx={{ display: 'flex' }}>
@@ -18,7 +38,62 @@ const Doctors = () => {
       
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
-        <h1>Doctor</h1>
+        <Typography variant="h5" sx={{ marginBottom: 5, fontWeight: 500 }}>
+             Doctors List
+          </Typography>
+          <Paper sx={{ width: "100%", overflow: "hidden" }}>
+            <TableContainer sx={{ maxHeight: 440 }}>
+              <Table stickyHeader aria-label="sticky table">
+                {doctor && doctor.length > 0 ? (
+                  <>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Full Name</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Phone No</TableCell>
+                        <TableCell>Document</TableCell>
+                        <TableCell>Action</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {doctor.map((value) => (
+                        <TableRow key={value._id}>
+                          <TableCell>{value.name}</TableCell>
+                          <TableCell>{value.email}</TableCell>
+                          <TableCell>{value?.phone}</TableCell>
+                          <TableCell>{}</TableCell>
+                          <TableCell>
+                            <Button
+                              variant="contained"
+                              color="success"
+                              sx={{ marginRight: 2 }}
+                              
+                            >
+                              Accept
+                            </Button>
+                            <Button
+                              variant="contained"
+                              color="error"
+                              sx={{ marginRight: 2 }}
+                              
+                            >
+                              Reject
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </>
+                ) : (
+                  <Box display={"flex"} justifyContent={"center"}>
+                    <Typography fontWeight={400} variant="h6">
+                      Currently there is no new application for doctor
+                    </Typography>
+                  </Box>
+                )}
+              </Table>
+            </TableContainer>
+          </Paper>
        
       </Box>
       </Box>
