c7f2627b1a82b3969659c94b83521491a3283587
abrar aboobacker <abraraboobackerkk@gmail.com>
2023-06-14 16:53:09 +0530

    change the constants

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,2 +1,2 @@
-// export const  baseURL = 'https://procare.onrender.com'
-export const  baseURL = 'http://localhost:3001'
+export const  baseURL = 'https://procare.onrender.com'
+// export const  baseURL = 'http://localhost:3001'
